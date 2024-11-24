bb47cb98b7f553fb52be1a99c51d73be3642c095
egebakisoglu <egebakisoglu@gmail.com>
2024-11-24 17:52:26 +0100

    changed nested if into elif

--- W5/extract.sh
+++ W5/extract.sh
@@ -2,23 +2,20 @@
 
 i=0
 if [ $# -eq 0 ]; then
-	while read -r line; do
+	while IFS= read -r line; do
 		i=`expr $i + 1`
-		if ! [ -z "`echo $line | cut -d: -f4`" ]; then
+		if [ -n "`echo $line | cut -d: -f4`" ]; then
+			echo "$i $line"
+		fi		
+	done
+elif [ $# -eq 1 -a -f $1 ]; then
+	while IFS= read -r line; do
+		i=`expr $i + 1`
+		if [ "-n `echo $line | cut -d: f4`" ]; then
 			echo "$i $line"
 		fi
-	done
+	done < $1
 else
-	if [ $# -eq 1 -a -f $1 ]; then
-		while read -r line; do
-			i=`expr $i + 1`
-			if ! [ -z "`echo $line | cut -d: -f4`" ]; then
-				echo "$i $line"
-			fi  
-		done < $1
-	else
-		echo "n1: $1 is not a regular file or more than one argument was given"
-		exit 1
-	fi
-fi
-exit 0
+	echo "extract: $1 is not a regular file or more than argument was given"
+	exit 1
+fi	
