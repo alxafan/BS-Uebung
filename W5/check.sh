9f2fd4f3a623aa4641faab708e6bf48e0e9f5d0c
egebakisoglu <egebakisoglu@gmail.com>
2024-11-24 20:14:06 +0100

    update check.sh
    cut -s => used to eliminate lines that may be given without ":" because of the custom text
    
    tr ',' '\n' => to separate the multiple users (if there are) and put them to different lines

--- W5/check.sh
+++ W5/check.sh
@@ -1,30 +1,11 @@
 #!/bin/sh
 
-if test $# -eq 0; then
-	while read -r line; do
-		for users in "`echo $line | cut -d: -f4`"; do
-			for user in ${users//,/ }; do
-				if ! cat /etc/passwd | grep "$user" > /dev/null ; then
-					echo "$user $line " 
-				fi
-			done
-		done
-	done
-else
-	if test $# -eq 1 -a -f $1; then
-		while read -r line; do
-			for users in "`echo $line | cut -d: -f4`"; do
-				for user in ${users//,/ }; do
-					if ! cat /etc/passwd | grep "$user" > /dev/null ; then
-						 echo "$user $line " 
-					fi
-				done
-			done
-		done < $1
-	else
-		echo "n1: $1 is not a regular file or more than one argument was given"
-		exit 1
-	fi
-fi
-exit 0
+while IFS= read -r line; do
+	users="`echo "$line" | cut -d: -f4 -s | tr ',' '\n'`"
 
+	for user in $users; do
+		if [ -z "`grep "^$user:" /etc/passwd`" ]; then
+		       echo "$user $line"
+		fi
+	done	
+done
